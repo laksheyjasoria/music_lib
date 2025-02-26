6676ee1d2a6a66db10646adb27163c410cb99805
Lakshey Kumar <139937109+laksheyjasoria@users.noreply.github.com>
2025-02-26 17:45:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 import yt_dlp
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/")
 def home():
     return "YouTube Audio Extractor API is Running!"
