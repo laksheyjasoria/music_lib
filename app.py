ff0f630633afb071adfedfb70109d23286abcd02
Lakshey Kumar <139937109+laksheyjasoria@users.noreply.github.com>
2025-03-04 22:45:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from collections import defaultdict
 app = Flask(__name__)
 CORS(app)
 
-YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+API_KEY = os.getenv("API_KEY")
 if not YOUTUBE_API_KEY:
-    raise ValueError("YOUTUBE_API_KEY is not set in environment variables.")
+    raise ValueError("API_KEY is not set in environment variables.")
 
 song_play_count = defaultdict(lambda: {"count": 0, "title": "", "thumbnail": ""})
 
