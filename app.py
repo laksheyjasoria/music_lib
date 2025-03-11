b68b165fdb83a027799a22c4d66f15bbafbba1e0
Lakshey Kumar <139937109+laksheyjasoria@users.noreply.github.com>
2025-03-11 22:07:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from collections import defaultdict
 from pydub import AudioSegment
 from io import BytesIO
 import re
+import audio
+immport utils
 
 app = Flask(__name__)
 CORS(app)
@@ -18,15 +20,6 @@ if not YT_API_KEY:
     raise ValueError("API_KEY is not set in environment variables.")
 
 song_play_count = defaultdict(lambda: {"count": 0, "title": "", "thumbnail": ""})
-
-def iso8601_to_seconds(duration):
-    match = re.match(r'PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?', duration)
-    if not match:
-        return 0
-    hours = int(match.group(1)) if match.group(1) else 0
-    minutes = int(match.group(2)) if match.group(2) else 0
-    seconds = int(match.group(3)) if match.group(3) else 0
-    return hours * 3600 + minutes * 60 + seconds
 
 def get_video_durations(video_ids):
     if not video_ids:
