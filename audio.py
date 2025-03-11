e34b478e1422092729f7c87bb6f03715d07425ab
Lakshey Kumar <139937109+laksheyjasoria@users.noreply.github.com>
2025-03-11 20:40:27 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,4 +1,5 @@
 import yt_dlp
+import requests
 
 def get_audio_url(video_id):
     """Fetches the best audio URL for a given YouTube video ID."""
@@ -10,3 +11,16 @@ def get_audio_url(video_id):
     except Exception as e:
         print(f"Error extracting audio URL: {e}")
         return None
+
+def get_video_durations_by_ids(video_ids):
+    if not video_ids:
+        return []
+
+    video_ids_str = ",".join(video_ids)
+    details_url = f"https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id={video_ids_str}&key={YT_API_KEY}"
+    details_response = requests.get(details_url).json()
+
+    return [
+        iso8601_to_seconds(item["contentDetails"]["duration"])
+        for item in details_response.get("items", [])
+    ]
