efb88778b87f8fe122eb73523b9ccba75ab4c1e8
Lakshey Kumar <139937109+laksheyjasoria@users.noreply.github.com>
2025-03-11 22:13:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,16 +119,7 @@ def get_trending_music():
 
     return jsonify({"trending_music": cached_trending_music})
 
-def get_audio_url(video_id):
-    """Fetches the best audio URL for a given YouTube video ID."""
-    try:
-        ydl_opts = {"format": "bestaudio/best", "noplaylist": True, "quiet": True}
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(f"https://www.youtube.com/watch?v={video_id}", download=False)
-            return info_dict["url"]
-    except Exception as e:
-        print(f"Error extracting audio URL: {e}")
-        return None
+
 @app.route("/get_most_played_songs", methods=["GET"])
 def get_most_played_songs():
     sorted_songs = sorted(song_play_count.items(), key=lambda x: x[1]["count"], reverse=True)
