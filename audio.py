9d7bd8922a53a5f7bea22c9459432414900f5f94
Lakshey Kumar <139937109+laksheyjasoria@users.noreply.github.com>
2025-03-20 19:55:07 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,16 +1,24 @@
 import yt_dlp
 import requests
 
+# def get_audio_url(video_id):
+#     """Fetches the best audio URL for a given YouTube video ID."""
+#     try:
+#         ydl_opts = {"format": "bestaudio/best", "noplaylist": True, "quiet": True}
+#         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#             info_dict = ydl.extract_info(f"https://www.youtube.com/watch?v={video_id}", download=False)
+#             return info_dict["url"]
+#     except Exception as e:
+#         print(f"Error extracting audio URL: {e}")
+#         return None
+
 def get_audio_url(video_id):
-    """Fetches the best audio URL for a given YouTube video ID."""
-    try:
-        ydl_opts = {"format": "bestaudio/best", "noplaylist": True, "quiet": True}
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(f"https://www.youtube.com/watch?v={video_id}", download=False)
-            return info_dict["url"]
-    except Exception as e:
-        print(f"Error extracting audio URL: {e}")
-        return None
+    url = f"https://www.youtube.com/watch?v={video_id}"
+    ydl_opts = {'format': 'bestaudio'}
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        return info['url']
 
 def get_video_durations_by_ids(video_ids):
     if not video_ids:
