8b58ed95a5c636763f92fd1c7a9bb0a2f3062933
Lakshey Kumar <139937109+laksheyjasoria@users.noreply.github.com>
2025-05-04 21:56:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 class Config:
     YT_API_KEY = os.getenv("API_KEY")
@@ -6,5 +7,8 @@ class Config:
     TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
     DEFAULT_FILE_ID = '18tMZ36WoVNOA-JvdGgFhq4cYdqsMU66Q'
     DEFAULT_FILENAME = 'cookies.json'
-    
+    TRENDING_CACHE_TTL = timedelta(hours=24)
+    MAX_TRENDING_RESULTS = 30
+    MAX_PLAY_COUNTS = 50
+
 config = Config()
